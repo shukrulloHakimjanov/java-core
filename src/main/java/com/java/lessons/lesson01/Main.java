7d1fe7831ade474075b6d16d9ea431e52c6f0db0
shukrulloHakimjanov <hakimjanovshukrullo0@gmail.com>
2025-09-25 12:29:11 +0500

    first commit

---
+++ src/main/java/com/java/lessons/lesson01/Main.java
@@ -0,0 +1,15 @@
+package com.java.lessons.lesson01;
+
+public class Main {
+
+    int value = 5;
+    String name = "Mike";
+
+    static final int DEFAULT_VALUE = 0;
+    static final String DEFAULT_NAME = "John";
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Math.abs(1);
+    }
+}
